eb9874e10e538c069313f9323b7c4378eaa22de1
ateexD <ateendraramesh@gmail.com>
2017-05-28 03:34:17 +0530

    try3

--- mails.php
+++ mails.php
@@ -30,5 +30,19 @@ if( empty($errors))
 	$headers .= "Reply-To: $email_address";
 
 	mail($to,$email_subject,$email_body,$headers);
+header('Location: index.html');
 }
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+</body>
+</html>
