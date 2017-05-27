dbcf4155fa53314a29e7b5c89a35c63638975136
ateexD <ateendraramesh@gmail.com>
2017-05-28 03:32:08 +0530

    try2

--- mails.php
+++ mails.php
@@ -32,16 +32,3 @@ if( empty($errors))
 	mail($to,$email_subject,$email_body,$headers);
 }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-	<title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
-<?php
-echo nl2br($errors);
-?>
-</body>
-</html>
