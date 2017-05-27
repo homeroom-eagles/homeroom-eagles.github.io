817ef1f3fb5468b34d6a2350a91b57f2b38edf5f
ateexD <ateendraramesh@gmail.com>
2017-05-28 03:46:09 +0530

    q2

--- mails.php
+++ mails.php
@@ -33,16 +33,3 @@ mail($to,"Query",$email_body,$headers);
 header('Location: index.html');
 }
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-	<title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
-<?php
-echo nl2br($errors);
-?>
-</body>
-</html>
